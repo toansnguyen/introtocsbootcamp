39de2ab64eb52ea5e5e6f8c092082cb1d2f18148
toansnguyen <toan.nguy@berkeley.edu>
2017-12-22 20:53:51 -0800

    assignment

---
+++ ch05/biggerBetterFavoriteNumber.rb
@@ -0,0 +1,4 @@
+puts 'Hello! What is your favorite number?'
+favnumber= gets.chomp.to_i
+newnumber= (favnumber+1).to_s
+puts 'That is an awesome favorite number, but do you know what number is bigger and better? '+ newnumber +'!'
---
+++ ch05/fullNameGreeting.rb
@@ -0,0 +1,5 @@
+puts 'Hi! What is your first name?'
+firstname= gets.chomp
+puts 'It is nice to meet you '+ firstname +'! What is your last name?'
+lastname= gets.chomp
+puts ''+ firstname +' '+ lastname +'? That is a wonderful name!'
