9747caeddc73ba82403cb4d852fc30fb3871bb83
toansnguyen <toan.nguy@berkeley.edu>
2017-12-24 16:58:36 -0800

    assignment

--- ch05/fullNameGreeting.rb
+++ ch05/fullNameGreeting.rb
@@ -2,4 +2,6 @@ puts 'Hi! What is your first name?'
 firstname= gets.chomp
 puts 'It is nice to meet you '+ firstname +'! What is your last name?'
 lastname= gets.chomp
-puts ''+ firstname +' '+ lastname +'? That is a wonderful name!'
+puts ''+ firstname +' '+ lastname +'? That is a wonderful name! What is your middle name?'
+middlename= gets.chomp
+puts 'So '+ firstname +' '+middlename+' '+lastname+' is your full name? You have a fantastic name!'
